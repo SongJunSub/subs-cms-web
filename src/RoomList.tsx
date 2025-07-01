8ae85fc2c2f4df842acf85b6faab00f9a6f0c4fc
JunSub_Dev <song911130@gmail.com>
2025-07-01 16:53:14 +0900

    feat: RoomList 컴포넌트에서 Room API 연동 구현

--- src/RoomList.tsx
+++ src/RoomList.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type Room = {
+  id: number;
+  name: string;
+  capacity: number;
+  price: number;
+  description: string;
+};
 
 function RoomList() {
-  // TODO: API 연동 및 상태 관리
+  const [rooms, setRooms] = useState<Room[]>([]);
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/rooms')
+      .then(res => setRooms(res.data))
+      .catch(() => setRooms([]));
+  }, []);
   return (
     <div>
-      <h2>객실 리스트 (샘플)</h2>
+      <h2>객실 리스트 (API 연동)</h2>
       <ul>
-        <li>101호 - 2인실</li>
-        <li>102호 - 4인실</li>
+        {rooms.map(room => (
+          <li key={room.id}>{room.name} - {room.capacity}인실 - {room.price.toLocaleString()}원</li>
+        ))}
       </ul>
     </div>
   );
