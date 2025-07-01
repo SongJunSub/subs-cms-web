8c9c7fd43f95c7103538b44bd42e58641f19355c
JunSub_Dev <song911130@gmail.com>
2025-07-01 16:44:07 +0900

    feat: 객실 리스트 샘플 컴포넌트(RoomList) 추가

---
+++ src/RoomList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function RoomList() {
+  // TODO: API 연동 및 상태 관리
+  return (
+    <div>
+      <h2>객실 리스트 (샘플)</h2>
+      <ul>
+        <li>101호 - 2인실</li>
+        <li>102호 - 4인실</li>
+      </ul>
+    </div>
+  );
+}
+
+export default RoomList;
+
